c11d122f48fa34c041ec8c920b321622ac32a8f6
Sergio Flores <sergio.flores.92@hotmail.com>
2017-01-24 15:35:50 -0600

    TxD works

--- P1-Serial/src/main.c
+++ P1-Serial/src/main.c
@@ -6,35 +6,40 @@ Description: Serial a patin
 #include <avr/io.h>
 #include <avr/wdt.h>
 
-#define TB     26  
-#define HIGH 0x10
+#define TB 13  
+#define HIGH 0x20
 #define LOW  0x00      
 //TB(tiempo de bit) = 104us that invertionally proportional to 9615 bit/s
 //Prescaler 16Mhz/64 = 250khz (4us) 
-
 
 void UART_init(void);
 void UART_send(unsigned char data);
 void setup_timer(void);
 
-unsigned int value_OUTCOMP=0;
-unsigned char OUTCOMP_flag=0;
-
 void UART_init(void)
 {
-	PORTD=0x10;		//set high pin 6 of PORT D
-	DDRD=0x10;		//configure pin 6 as output
+	PORTD=0x20;		//set high pin 6 of PORT D
+	DDRD=0x20;		//configure pin 6 as output
 }
 
 void UART_send(unsigned char data)
 {
-	unsigned char cont = 8;			//used to count the send bits
+	unsigned char volatile cont;			//used to count the send bits
+	unsigned int volatile value_OUTCOMP;
+	unsigned char volatile OUTCOMP_flag;
+	
+	cont = 8;
+	value_OUTCOMP=0;
+	OUTCOMP_flag=0;
+	
 	value_OUTCOMP=TCNT1+TB;			//time for next send
 	PORTD=0x00;						//set low pin 6 of PORT D (START BIT)
 	do{
 		wdt_reset();
 		OUTCOMP_flag = (TCNT1 >= value_OUTCOMP) ? 1:0;
 	}while(OUTCOMP_flag==0);
+	OUTCOMP_flag=0;
+	
 	
 	do{
 		value_OUTCOMP+=TB;					//Update next send time
@@ -54,7 +59,7 @@ void UART_send(unsigned char data)
 		wdt_reset();
 		OUTCOMP_flag = (TCNT1 >= value_OUTCOMP) ? 1:0;
 	}while(OUTCOMP_flag==0);				
-	
+	TCNT1 =0;
 	
 }
 void setup_timer(void)
@@ -62,7 +67,7 @@ void setup_timer(void)
 	TCCR1B = (1<<CS11)|(1<<CS10);    //1:64 prescaler (meterlo en timer_init)
 }
 
-void main(void)
+int main(void)
 {
 	UART_init();
 	setup_timer();
@@ -70,7 +75,12 @@ void main(void)
 	for(;;)
 	{
 	    //Read timer value and act according with it
-		UART_send(0xa0);
-		
+		UART_send(0x50);
+		UART_send(0x25);
+		UART_send(0x41);
+		UART_send(0x0A);
+		UART_send(0x0D);
     }
+	return 0;
 }
+
