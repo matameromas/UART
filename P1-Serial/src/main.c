86b9fef3c5601ab217f5e6d4915c7b18fcc36882
Sergio Flores <sergio.flores.92@hotmail.com>
2017-01-24 17:23:52 -0600

    interrupciones no funcionan

--- P1-Serial/src/main.c
+++ P1-Serial/src/main.c
@@ -1,20 +1,33 @@
 /*
 Author: Luis Israel Rivera Rodriguez , Daniela Becerra, Sergio Flores
 Date:   Jan, 2017
-Description: Serial a patin 
+Description: Software made UART
 */
+
+/************* Includes *************/
 #include <avr/io.h>
 #include <avr/wdt.h>
+#include <avr/interrupt.h>
 
+//TB(tiempo de bit) = 104us that invertionally proportional to 9615 bit/s
+//Prescaler 16Mhz/64 = 250khz (4us)
 #define TB 13  
 #define HIGH 0x20
-#define LOW  0x00      
-//TB(tiempo de bit) = 104us that invertionally proportional to 9615 bit/s
-//Prescaler 16Mhz/64 = 250khz (4us) 
+#define LOW  0x00
+#define DATA_LENGTH 10 
+#define STOP_BIT	1
+typedef unsigned char u8;     
 
+
+/************* Function Prototypes *************/
 void UART_init(void);
 void UART_send(unsigned char data);
-void setup_timer(void);
+void TIMER_init(void);
+
+/*******   Global variables ****************/
+u8 THR;							//Transmit holding register
+u8 volatile cont;				//used to count the bits we'll send
+
 
 void UART_init(void)
 {
@@ -22,62 +35,54 @@ void UART_init(void)
 	DDRD=0x20;		//configure pin 6 as output
 }
 
+ISR(TIMER1_COMPA_vect){						//This is our timer interrupt service routine
+	// the output compare flag is automatically cleared when the interrupt is executed
+	OCR1A+=TB;
+	cont--;
+	if((cont<=DATA_LENGTH) && (cont!=STOP_BIT))
+	{
+		PORTD = THR & 0x01 ? HIGH : LOW;		//mask to LSB to send 1 by 1 bit
+		THR>>=1;								//shift to send next bit
+	}
+	if(cont==STOP_BIT)
+	{
+		PORTD = HIGH;
+	}
+	if(cont==0)
+	{
+		TIMSK1 = (0<<OCIE1A);						//Disable timer interrupts, we're done
+	}
+	wdt_reset();									//reset watchdog
+}
+
 void UART_send(unsigned char data)
 {
-	unsigned char volatile cont;			//used to count the send bits
-	unsigned int volatile value_OUTCOMP;
-	unsigned char volatile OUTCOMP_flag;
-	
-	cont = 8;
-	value_OUTCOMP=0;
-	OUTCOMP_flag=0;
-	
-	value_OUTCOMP=TCNT1+TB;			//time for next send
-	PORTD=0x00;						//set low pin 6 of PORT D (START BIT)
-	do{
-		wdt_reset();
-		OUTCOMP_flag = (TCNT1 >= value_OUTCOMP) ? 1:0;
-	}while(OUTCOMP_flag==0);
-	OUTCOMP_flag=0;
-	
-	
-	do{
-		value_OUTCOMP+=TB;					//Update next send time
-		PORTD = data & 0x01? HIGH : LOW;	//mask to LSB to send 1 by 1 bit
-		data>>=1;							//shift to send next bit
-		OUTCOMP_flag=0;						
-		do{
-			wdt_reset();
-			OUTCOMP_flag = (TCNT1 >= value_OUTCOMP) ? 1:0;
-		}while(OUTCOMP_flag==0);
-		
-	}while(--cont);							//secure 8 bits are send
-	
-	PORTD = HIGH;							//STOP
-	value_OUTCOMP += TB;
-	do{
-		wdt_reset();
-		OUTCOMP_flag = (TCNT1 >= value_OUTCOMP) ? 1:0;
-	}while(OUTCOMP_flag==0);				
-	TCNT1 =0;
-	
+	cont=DATA_LENGTH;								//8 bits plus start and stop bit
+	OCR1A=TCNT1+TB;									//time for next send
+	PORTD=0x00;										//set low pin 6 of PORT D (START BIT)
+	THR=data;										//put the data in the global variable THR
+	wdt_reset();									//reset watchdog
+	TIMSK1 = (1<<OCIE1A);							//Enable timer interrupts	
 }
-void setup_timer(void)
-{
-	TCCR1B = (1<<CS11)|(1<<CS10);    //1:64 prescaler (meterlo en timer_init)
+
+void TIMER_init(void)
+{												// CTC mode allows for automatic OutComp
+	TCCR1B = (1<<WGM12)|(1<<CS11)|(1<<CS10);    //Enable CTC mode, and set prescaler to 1:64
 }
 
 int main(void)
 {
 	UART_init();
-	setup_timer();
+	TIMER_init();
+	sei();										//Enable global interrupts
 	
 	for(;;)
 	{
 	    //Read timer value and act according with it
-		UART_send(0x50);
-		UART_send(0x25);
-		UART_send(0x41);
+		UART_send(0x45);
+		UART_send(0x72);
+		UART_send(0x4C);
+		UART_send(0x6f);
 		UART_send(0x0A);
 		UART_send(0x0D);
     }
